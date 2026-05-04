d0808ea567110ad56b45fd15c26d58e72fe0e0a4
Camille-lt <camillelebigot5@gmail.com>
2026-05-04 13:26:55 +0200

    updated style of footer

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -23,30 +23,30 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-<html
-  lang="fr"
-  className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
->
-  <body className="min-h-full flex flex-col">
-    {/* Le contenu principal prend tout l'espace disponible (flex-1) */}
-    <main className="flex-1">
-      {children}
-    </main>
-    {/* Le footer reste en bas grâce au flex-col du body */}
-    <footer className="bg-brand-blue py-8 px-6 border-t rounded-t-[50px] border-blue-400"> 
-      <div className="ml-5 mr-5 text-center mx-auto">
-        <h1 className="text-white font-extrabold tracking-tight uppercase">
-          OPEN GO PARIS
-        </h1>
-        <p className="text-white ">
-          API Opendata.paris - Que Faire à Paris ? - Evènements et activités
-        </p>
-        <p className="text-blue-200 text-[10px] mt-2 uppercase tracking-widest">
-          © by Camille LEBIGOT
-        </p>
-      </div>
-    </footer>
-  </body>
-</html>
+    <html
+      lang="fr"
+      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
+    >
+      <body className="min-h-full flex flex-col">
+        {/* Le contenu principal prend tout l'espace disponible (flex-1) */}
+        <main className="flex-1">
+          {children}
+        </main>
+        {/* Le footer reste en bas grâce au flex-col du body */}
+        <footer className="bg-brand-blue pt-5 pb-5 px-5 sm:pt-10 sm:pb-10 sm:px-6 border-t rounded-t-[50px] border-blue-400 mt-auto">
+          <div className="max-w-md mx-auto text-center">
+            <h1 className="text-white font-extrabold tracking-tight uppercase text-lg sm:text-2xl">
+              OPEN GO PARIS
+            </h1>
+            <p className="text-white text-xs sm:text-sm mt-2">
+              API Opendata.paris - Que Faire à Paris ?
+            </p>
+            <p className="text-blue-200 text-[9px] sm:text-[10px] mt-3 uppercase tracking-widest">
+              © by Camille LEBIGOT
+            </p>
+          </div>
+        </footer>
+      </body>
+    </html>
   );
 }
